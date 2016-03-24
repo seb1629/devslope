3e5231818732497ca78b5d38283bb148113de061
SEBASTIEN MONTIBELLER <sebastienmontibeller@sebastiens-macbook.local>
2016-03-24 23:18:54 +1100

    Initial Commit

---
+++ DevslopeShowcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevslopeShowcase
+//
+//  Created by SEBASTIEN MONTIBELLER on 24/03/2016.
+//  Copyright © 2016 SEBASTIEN MONTIBELLER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
